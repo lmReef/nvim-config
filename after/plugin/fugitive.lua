2b29d953c8ff4c95b5b15177f723eb79a2f84441
Reef <reefmatson@gmail.com>
2024-03-07 16:12:31 +0700

    random mintty diff and update fugitive leader cmds

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,3 @@
-vim.keymap.set('n', '<leader>gs', '<cmd>:tab G<CR>')
+vim.keymap.set('n', '<leader>gs', '<cmd>:tab Git<CR>')
+vim.keymap.set('n', '<leader>gb', '<cmd>:tab Git blame<CR>')
+vim.keymap.set('n', '<leader>gd', '<cmd>:Gdiffsplit<CR>')
