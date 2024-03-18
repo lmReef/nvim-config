c44a56669d9d04067e56c0ccc7d6937337413471
Reef <reefmatson@gmail.com>
2024-03-18 10:36:54 +0700

    fix var not in scope issue

--- lua/reef/remap_debug.lua
+++ lua/reef/remap_debug.lua
@@ -1,10 +1,26 @@
 local run_handler = function()
     local filetype = vim.bo.filetype
+
     if filetype == 'rust' then
         vim.cmd(':!cargo r')
+    elseif filetype == 'javascript' then
+        vim.cmd(':!node index.js')
     else
         print('No run handler set for filetype', filetype)
     end
 end
 
+local build_handler = function()
+    local filetype = vim.bo.filetype
+
+    if vim.fn.filereadable('./build.bat') then
+        vim.cmd(':!build.bat')
+    elseif filetype == 'rust' then
+        vim.cmd(':!cargo b --release')
+    else
+        print('Could not find build.bat and no build handler set for filetype', filetype)
+    end
+end
+
 vim.keymap.set('n', '<leader>r', run_handler)
+vim.keymap.set('n', '<leader>b', build_handler)
