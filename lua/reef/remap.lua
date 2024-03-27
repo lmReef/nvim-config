eb2a884a8bb41db7faf2dcb511b6e3e5a35005b4
Reef <reefmatson@gmail.com>
2024-03-27 15:23:56 +0700

    comment out broken keymaps for paste, remove unused compile func

--- lua/reef/remap.lua
+++ lua/reef/remap.lua
@@ -13,8 +13,9 @@ vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
--- paste and move old to void
-vim.keymap.set('x', '<leader>p', "\"_dP")
+-- paste from system clipboard
+-- vim.keymap.set('x', '<leader>p', '\"+p')
+-- vim.keymap.set('x', '<leader>p', '\"+P')
 
 -- yank to system clipboard
 vim.keymap.set('n', '<leader>y', "\"+y")
@@ -25,7 +26,7 @@ vim.keymap.set('n', '<leader>d', "\"_d")
 vim.keymap.set('n', '<leader>d', "\"_d")
 
 -- for swapping between projects/folders
--- vim.keymap.set('n', '<C-f>', '<cmd>!tmux neww tmux-sessionizer<CR>')
+-- vim.keymap.set('n', '<C-f>', "<cmd>!tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
@@ -34,18 +35,3 @@ vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
--- compile
-local function custom_compile()
-    -- save first
-    vim.cmd('w')
-
-    local buffer_path = vim.fn.expand('%')
-
-    if string.find(buffer_path, '.cpp') then
-        vim.cmd('!g++ %:p')
-    else
-        print('No matching compiler found for current file.')
-    end
-end
-vim.keymap.set('n', '<leader>q', custom_compile)
