23bad4a407d3294a0443afcff8c34f8120e293e0
lmReef <ReefMatson@gmail.com>
2024-04-04 01:32:31 +0700

    tweak lualine

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -15,12 +15,25 @@ require('lualine').setup {
 			statusline = 1000,
 			tabline = 1000,
 			winbar = 1000,
-		}
+		},
 	},
 	sections = {
 		lualine_a = { 'mode' },
 		lualine_b = { 'branch', 'diff', 'diagnostics' },
-		lualine_c = { 'filename' },
+		lualine_c = { 
+			{
+				'filename',
+				file_status = true,
+				newfile_status = true,
+				path = 1,
+				symbols = {
+					modified = '[+]',      -- Text to show when the file is modified.
+					readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
+					unnamed = '[No Name]', -- Text to show for unnamed buffers.
+					newfile = '[New]',     -- Text to show for newly created file before first write
+				}
+			}
+		},
 		lualine_x = { 'encoding', 'fileformat', 'filetype' },
 		lualine_y = { 'progress' },
 		lualine_z = { 'location' }
