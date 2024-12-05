284901964d2ff2f76f23b4704a58601b44ac0990
lmReef <ReefMatson@gmail.com>
2024-12-06 10:32:58 +1300

    add telescope git branches map

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,8 +1,8 @@
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>pc', builtin.git_branches, {})
 vim.keymap.set('n', '<leader>pg', function()
 	vim.cmd(':Easypick Changed Files')
 end, {})
-
-vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
