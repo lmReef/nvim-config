ee16e9cd48a5148ca72e815dcd25cf24dda01c22
lmReef <ReefMatson@gmail.com>
2025-01-31 11:23:09 +1300

    remove dep

--- lua/plugins/essentials.lua
+++ lua/plugins/essentials.lua
@@ -19,7 +19,7 @@ return {
             { "hrsh7th/nvim-cmp" },
             { "hrsh7th/cmp-nvim-lua" },
             { "L3MON4D3/LuaSnip" },
-            { "saadparwaiz1/cmp_luasnip",         dependencies = { "kmarius/jsregexp" } },
+            { "saadparwaiz1/cmp_luasnip" },
             { "williamboman/mason.nvim" },
             { "williamboman/mason-lspconfig.nvim" },
             { "rafamadriz/friendly-snippets" },
