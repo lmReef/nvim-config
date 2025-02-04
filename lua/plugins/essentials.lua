f1e0d9e415093da52964c31327ada0315777a891
lmReef <ReefMatson@gmail.com>
2025-02-04 14:32:13 +1300

    add tmux sessions nvim

--- lua/plugins/essentials.lua
+++ lua/plugins/essentials.lua
@@ -1,68 +1,72 @@
 return {
-    "luarocks/luarocks",
-    "nvim-lua/plenary.nvim",
-    "onsails/lspkind.nvim",
-    "nvim-treesitter/nvim-treesitter",
-    "sbdchd/neoformat",
+	"luarocks/luarocks",
+	"nvim-lua/plenary.nvim",
+	"onsails/lspkind.nvim",
+	"nvim-treesitter/nvim-treesitter",
+	"sbdchd/neoformat",
 
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        dependencies = {
-            { "neovim/nvim-lspconfig" },
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-path" },
-            { "hrsh7th/cmp-cmdline" },
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-nvim-lua" },
-            { "L3MON4D3/LuaSnip" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
-            { "rafamadriz/friendly-snippets" },
-        },
-    },
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		dependencies = {
+			{ "neovim/nvim-lspconfig" },
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "hrsh7th/cmp-cmdline" },
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+			{ "L3MON4D3/LuaSnip" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	},
 
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "BurntSushi/ripgrep",
-            "nvim-telescope/telescope-fzf-native.nvim",
-            -- TODO: can probably implement this without the plugin
-            {
-                "axkirillov/easypick.nvim",
-                config = function()
-                    local easypick = require("easypick")
-                    easypick.setup({
-                        pickers = {
-                            {
-                                name = "Changed Files",
-                                command = "git diff --name-only",
-                                previewer = easypick.previewers.file_diff(),
-                            },
-                        },
-                    })
-                end,
-            },
-        },
-    },
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"BurntSushi/ripgrep",
+			"nvim-telescope/telescope-fzf-native.nvim",
+			{
+				"axkirillov/easypick.nvim",
+				config = function()
+					local easypick = require("easypick")
+					easypick.setup({
+						pickers = {
+							{
+								name = "Changed Files",
+								command = "git diff --name-only",
+								previewer = easypick.previewers.file_diff(),
+							},
+							{
+								name = "tmux-ls",
+								command = "tmux ls | grep -o -P '^.*(?=: )'",
+								action = easypick.actions.nvim_command(":silent !tmux switch-client -t"),
+							},
+						},
+					})
+				end,
+			},
+		},
+	},
 
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { "nvim-lua/plenary.nvim" },
-    },
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 
-    {
-        "rcarriga/nvim-notify",
-        opts = {
-            render = "wrapped-compact",
-            stages = "fade_in_slide_out",
-            fps = 90,
-            timeout = 3500,
-            background_colour = "#999999",
-        },
-    },
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			render = "wrapped-compact",
+			stages = "fade_in_slide_out",
+			fps = 90,
+			timeout = 3500,
+			background_colour = "#999999",
+		},
+	},
 }
