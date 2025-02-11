eb64b17fd6bf3beadd3c67dbd873608f59ccfcd1
lmReef <ReefMatson@gmail.com>
2025-02-11 14:54:40 +1300

    move telescope

--- lua/plugins/essentials.lua
+++ lua/plugins/essentials.lua
@@ -51,6 +51,22 @@ return {
 				end,
 			},
 		},
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>ps", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>pc", builtin.git_branches, {})
+			vim.keymap.set("n", "<leader>pt", function()
+				vim.cmd(":Easypick tmux-ls")
+			end)
+			vim.keymap.set("n", "<leader>pg", function()
+				vim.cmd(":Easypick Changed Files")
+			end, {})
+			vim.keymap.set("n", "<leader>pn", function()
+				vim.cmd(":Telescope notify")
+			end)
+		end,
+		keys = "<leader>p",
 	},
 
 	{
