9b2c2385d94222ec4b8c26f1538ddcab13b77bab
lmReef <ReefMatson@gmail.com>
2025-02-11 16:08:30 +1300

    move undotree + fix, formatting

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -1,38 +1,48 @@
 return {
-    "brenoprata10/nvim-highlight-colors",
-    "mbbill/undotree",
-    "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
-    "simrat39/symbols-outline.nvim",
+	"brenoprata10/nvim-highlight-colors",
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	"simrat39/symbols-outline.nvim",
 
-    { "nextflow-io/vim-language-nextflow", ft = "nextflow" },
-    { "vimwiki/vimwiki",                   keys = "<leader>w" },
+	{ "nextflow-io/vim-language-nextflow", ft = "nextflow" },
+	{ "vimwiki/vimwiki", keys = "<leader>w" },
 
-    {
-        "windwp/nvim-ts-autotag",
-        opts = {},
-        ft = {
-            "astro",
-            "glimmer",
-            "handlebars",
-            "html",
-            "javascript",
-            "jsx",
-            "markdown",
-            "php",
-            "rescript",
-            "svelte",
-            "tsx",
-            "twig",
-            "typescript",
-            "vue",
-            "xml",
-        },
-    },
+	{
+		"mbbill/undotree",
+		config = function()
+			vim.keymap.set("n", "<leader>u", function()
+				vim.cmd.UndotreeToggle()
+				vim.cmd.UndotreeFocus()
+			end)
+		end,
+		keys = "<leader>u",
+	},
 
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("ibl").setup()
-        end,
-    },
+	{
+		"windwp/nvim-ts-autotag",
+		opts = {},
+		ft = {
+			"astro",
+			"glimmer",
+			"handlebars",
+			"html",
+			"javascript",
+			"jsx",
+			"markdown",
+			"php",
+			"rescript",
+			"svelte",
+			"tsx",
+			"twig",
+			"typescript",
+			"vue",
+			"xml",
+		},
+	},
+
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		config = function()
+			require("ibl").setup()
+		end,
+	},
 }
