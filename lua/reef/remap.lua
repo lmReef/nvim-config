923d9682aa25217fa86a37b39a87257ef129218f
lmReef <ReefMatson@gmail.com>
2025-02-20 10:40:39 +1300

    set scrolloff 999

--- lua/reef/remap.lua
+++ lua/reef/remap.lua
@@ -8,8 +8,8 @@ vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
 
 -- nav and keep cursor in a good spot
 vim.keymap.set('n', 'J', 'mzJ`z')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
+-- vim.keymap.set('n', '<C-d>', '<C-d>zz')
+-- vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
 vim.keymap.set('n', 'N', 'Nzzzv')
 
--- lua/reef/set.lua
+++ lua/reef/set.lua
@@ -21,7 +21,7 @@ vim.opt.incsearch = true
 
 vim.opt.termguicolors = true
 
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 999
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
