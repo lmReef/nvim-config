dd17521860123f063f855f091cdb51f860b5c289
lmReef <ReefMatson@gmail.com>
2025-02-21 13:35:10 +1300

    git blame right align, formatting

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,74 +1,74 @@
 return {
-    "tpope/vim-fugitive",
-    "tpope/vim-rhubarb",
+	"tpope/vim-fugitive",
+	"tpope/vim-rhubarb",
 
-    {
-        "lewis6991/gitsigns.nvim",
-        opts = {
-            signs = {
-                add = { text = "│" },
-                change = { text = "│" },
-                delete = { text = "_" },
-                topdelete = { text = "‾" },
-                changedelete = { text = "~" },
-                untracked = { text = "┆" },
-            },
-            signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-            numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-            linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-            word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-            watch_gitdir = {
-                follow_files = true,
-            },
-            auto_attach = true,
-            attach_to_untracked = true,
-            on_attach = function(bufnr)
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "│" },
+				change = { text = "│" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+				untracked = { text = "┆" },
+			},
+			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
+			numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
+			linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+			word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
+			watch_gitdir = {
+				follow_files = true,
+			},
+			auto_attach = true,
+			attach_to_untracked = true,
+			on_attach = function(bufnr)
+				local function map(mode, l, r, opts)
+					opts = opts or {}
+					opts.buffer = bufnr
+					vim.keymap.set(mode, l, r, opts)
+				end
 
-                -- Navigation
-                map("n", "]c", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "]czz", bang = true })
-                    else
-                        gitsigns.nav_hunk("next")
-                        vim.cmd.normal({ "zz", bang = true })
-                    end
-                end)
+				-- Navigation
+				map("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]czz", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+						vim.cmd.normal({ "zz", bang = true })
+					end
+				end)
 
-                map("n", "[c", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "[czz", bang = true })
-                    else
-                        gitsigns.nav_hunk("prev")
-                        vim.cmd.normal({ "zz", bang = true })
-                    end
-                end)
-            end,
-            current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
-            current_line_blame_opts = {
-                virt_text = true,
-                virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-                delay = 500,
-                ignore_whitespace = false,
-                virt_text_priority = 100,
-            },
-            current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-            sign_priority = 6,
-            update_debounce = 100,
-            status_formatter = nil, -- Use default
-            max_file_length = 40000, -- Disable if file is longer than this (in lines)
-            preview_config = {
-                -- Options passed to nvim_open_win
-                border = "single",
-                style = "minimal",
-                relative = "cursor",
-                row = 0,
-                col = 1,
-            },
-        },
-    },
+				map("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[czz", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+						vim.cmd.normal({ "zz", bang = true })
+					end
+				end)
+			end,
+			current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+			current_line_blame_opts = {
+				virt_text = true,
+				virt_text_pos = "right_align", -- 'eol' | 'overlay' | 'right_align'
+				delay = 500,
+				ignore_whitespace = false,
+				virt_text_priority = 100,
+			},
+			current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+			sign_priority = 6,
+			update_debounce = 100,
+			status_formatter = nil, -- Use default
+			max_file_length = 40000, -- Disable if file is longer than this (in lines)
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "single",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+		},
+	},
 }
