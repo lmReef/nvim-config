d07208ab611884cc485e328ba864b68bcb0583a1
lmReef <ReefMatson@gmail.com>
2025-03-04 09:09:35 +1300

    add search word keymap

--- lua/reef/remap.lua
+++ lua/reef/remap.lua
@@ -31,6 +31,7 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+vim.keymap.set("n", "<C-_>", [[:/\<<C-r><C-w>\><CR>]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
