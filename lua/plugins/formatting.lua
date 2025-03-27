908d3692f05a210a583f3db147dec3aebb20d5a5
lmReef <ReefMatson@gmail.com>
2025-03-27 18:18:25 +1300

    merge conflicts

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -49,6 +49,19 @@ return {
 
 				rust = { require("formatter.filetypes.rust").rustfmt },
 
+				gdscript = {
+					function()
+						local util = require("formatter.util")
+						return {
+							exe = "gdformat",
+							args = {
+								util.escape_path(util.get_current_buffer_file_path()),
+							},
+							stdin = true,
+						}
+					end,
+				},
+
 				nextflow = {
 					function()
 						vim.lsp.buf.format()
