c98f1635b8a63adf447f708405b0e7c257c716ec
lmReef <ReefMatson@proton.me>
2025-05-08 14:23:06 +1200

    tweak keymaps

--- lua/reef/remap.lua
+++ lua/reef/remap.lua
@@ -1,45 +1,47 @@
 vim.g.mapleader = " "
 
 -- move selected blocks and indent
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<cr>gv=gv")
+vim.keymap.set("v", "J", ":m '>+1<cr>gv=gv")
 
 -- nav and keep cursor in a good spot
 vim.keymap.set("n", "J", "mzJ`z")
--- vim.keymap.set('n', '<C-d>', '<C-d>zz')
--- vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
-
--- paste from system clipboard
--- vim.keymap.set('x', '<leader>p', '\"+p')
--- vim.keymap.set('x', '<leader>p', '\"+P')
 
 -- yank to system clipboard
 vim.keymap.set("n", "<leader>y", '"+y')
 vim.keymap.set("v", "<leader>y", '"+y')
 vim.keymap.set("n", "<leader>Y", '"+Y')
 
+-- delete no hist
 vim.keymap.set("n", "<leader>d", '"_d')
 vim.keymap.set("n", "<leader>d", '"_d')
 
--- for swapping between projects/folders
--- vim.keymap.set('n', '<C-f>', "<cmd>!tmux neww tmux-sessionizer<CR>")
+vim.keymap.set("n", "<C-k>", ":cnext<cr>zz")
+vim.keymap.set("n", "<C-j>", ":cprev<cr>zz")
+vim.keymap.set("n", "<leader>k", ":lnext<cr>zz")
+vim.keymap.set("n", "<leader>j", ":lprev<cr>zz")
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<C-_>", [[:/\<<C-r><C-w>\><CR>]])
+vim.keymap.set("n", "<C-_>", [[:/\<<C-r><C-w>\><cr>]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
--- tmux sessionizer
--- vim.keymap.set('n', '<C-f>', ':!ls $HOME/<CR>')
+vim.keymap.set("n", "<leader>x", ":silent !chmod +x %<cr>")
 
 -- git
-vim.keymap.set("n", "<leader>gs", "<cmd>:tab Git<CR>")
-vim.keymap.set("n", "<leader>gb", "<cmd>:tab Git blame<CR>")
-vim.keymap.set("n", "<leader>gd", "<cmd>:Gdiffsplit<CR>")
-vim.keymap.set("n", "<leader>ga", ":lua MiniDiff.toggle_overlay()<CR>")
+vim.keymap.set("n", "<leader>gs", ":tab Git<cr>")
+vim.keymap.set("n", "<leader>gb", ":tab Git blame<cr>")
+vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<cr>")
+vim.keymap.set("n", "<leader>ga", ":lua MiniDiff.toggle_overlay()<cr>")
+vim.keymap.set("n", "<leader>gc", ":Git commit<cr>")
+
+-- file level binds
+vim.keymap.set("n", "<leader>fs", ":w<cr>")
+vim.keymap.set("n", "<leader>fa", ":Git add %<cr>")
+
+-- open links
+vim.keymap.set("n", "<cr>", function()
+	local word = vim.fn.expand("<cWORD>")
+	if string.find(word, "http") then
+		vim.cmd("silent !" .. os.getenv("BROWSER") .. " " .. word)
+	end
+end)
