cd3e96a3f8f2c5848409cab67db629975d106864
lmReef <ReefMatson@proton.me>
2025-05-08 16:25:47 +1200

    nextflow lsp

--- after/plugin/nextflow.lua
+++ after/plugin/nextflow.lua
@@ -8,7 +8,7 @@ lspconfig.nextflow_ls.setup({
 	cmd = {
 		"java",
 		"-jar",
-		os.getenv("HOME") .. "/projects/nextflow-lsp/build/libs/language-server-all.jar",
+		os.getenv("HOME") .. "/.nextflow/lsp/v24.10/v24.10.4.jar",
 	},
 	filetypes = { "nextflow" },
 	root_dir = lspconfig.util.root_pattern("nextflow.config", ".git"),
