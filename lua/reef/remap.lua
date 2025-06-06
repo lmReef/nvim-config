03dc15f8cbb01dc0f3fbdabc2f359f6b662cc837
lmReef <ReefMatson@proton.me>
2025-06-06 16:21:43 +1200

    tweak git keymaps

--- lua/reef/remap.lua
+++ lua/reef/remap.lua
@@ -38,12 +38,13 @@ vim.keymap.set("n", "<leader>q", ":q!<cr>", { desc = ":q!" })
 
 -- git
 vim.keymap.set("n", "<leader>gs", ":tab Git<cr>))", { desc = "Git status" })
-vim.keymap.set("n", "<leader>gb", ":tab Git blame<cr>", { desc = "Git blame" })
+vim.keymap.set("n", "<leader>gb", ":Git blame<cr>", { desc = "Git blame" })
 vim.keymap.set("n", "<leader>gd", ":Gdiffsplit<cr>", { desc = "Git diff side-by-side" })
 vim.keymap.set("n", "<leader>ga", ":lua MiniDiff.toggle_overlay()<cr>", { desc = "Git diff inline" })
 vim.keymap.set("n", "<leader>gc", ":Git commit<cr>", { desc = "Git commit" })
 vim.keymap.set("n", "<leader>gp", ":Git push<cr>", { desc = "Git push" })
 vim.keymap.set("n", "<leader>go", ":Git pull<cr>", { desc = "Git pull" })
+vim.keymap.set("n", "<leader>gg", ":GBrowse<cr>", { desc = "Open file in github" })
 vim.keymap.set("n", "<leader>fa", ":Git add %<cr>", { desc = "Git add" })
 
 -- open links
