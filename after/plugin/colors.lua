a314122435d62a374c1835bc5a823a2ae457e393
lmReef <ReefMatson@proton.me>
2025-07-05 12:20:56 +1200

    change default theme

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -20,7 +20,7 @@ function PywalTheme()
 end
 
 function Theme(color)
-	color = color or os.getenv("NVIM_THEME") or "vague"
+	color = color or os.getenv("NVIM_THEME") or "nightfox"
 	vim.cmd.colorscheme(color)
 
 	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
